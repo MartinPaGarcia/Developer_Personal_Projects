c76d9babd4735697f8721781e36c050a16b0f0a9
MartinPaGarcia <a01066569@itesm.mx>
2023-09-15 11:48:21 -0600

    Balanza

---
+++ Python/Tec/Semestre 8/Ciencia de Datos/Tareas/balanza.py
@@ -0,0 +1,50 @@
+import random 
+
+'''
+Autor: Martín Palomares García
+Fecha: 15/09/2023
+Matricula: A01066569
+Descripción: Programa que encuentra la piedra más ligera de 10 piedras utilizando una balanza.
+
+'''
+
+
+
+def piedras():
+    n = 10
+    piedras = []
+    for i in range(n):
+        piedras.append(random.randint(1, 1000))
+    return piedras
+
+def balanza(piedras):
+    n = len(piedras)
+    if n == 1:
+        return piedras[0]
+    elif n == 2:
+        if piedras[0] < piedras[1]:
+            return piedras[0]
+        else:
+            return piedras[1]
+    else:
+        mitad = n // 2
+        lado1 = balanza(piedras[:mitad])
+        lado2 = balanza(piedras[mitad:])
+        if lado1 < lado2:
+            return lado1
+        else:
+            return lado2
+
+
+def main():
+    print ("Balanza")
+    pesos = piedras()
+    print (f"Las piedras son: {pesos}")
+
+    piedra_mas_ligera = balanza(pesos)
+    print (f"La piedra más ligera es: {piedra_mas_ligera}")
+
+
+if __name__ == "__main__":
+    main()
+
