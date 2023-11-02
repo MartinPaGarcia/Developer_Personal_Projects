e1aa303c1d37fa795b33893fbb455991aacf3a73
MartinPaGarcia <a01066569@itesm.mx>
2023-11-01 22:46:09 -0600

    empty arrays removed

--- Swift/test.swift
+++ Swift/test.swift
@@ -1,30 +1,36 @@
-var fruit = ["apple", "banana", "orange"]  // Array of strings  
-print (fruit)
+// Arrays
+func fruits() {
+    let fruit = ["apple", "banana", "orange"]  // Array of strings
+    print(fruit)
+}
 
-
-// No information is required to assing an empty array or dictionary to a new variable, or another place
-// where there is no information, you need to specify the type
-
-let emptyArray:[String] = []
-let emptyDictionary:[String: Float] = [:]
+// Empty Arrays and Dictionaries
+func emptyArrays() {
+    // No information is required to assign an empty array or dictionary to a new variable, or another place
+    // where there is no information, you need to specify the type
+    // let emptyArray: [String] = []
+    // let emptyDictionary: [String: Float] = [:]
+}
 
 // Control Flow
-
- 
-// if and switch to make conditionals, used for loops and while to make loops, Parantheses around the conditions
-// or loop variable are optional. Braces around the body are required.
-
-
-let individualScores = [75, 43, 103, 87, 12]
-var teamScore = 0
-
-for score: Int in individualScores{
-    if score > 50 {
-        teamScore += 3
+func controlFlow() {
+    // Use if and switch to make conditionals, use for-in, while, and repeat-while to make loops. Parentheses
+    // around the condition or loop variable are optional. Braces around the body are required.
+    let individualScores = [75, 43, 103, 87, 12]
+    var teamScore = 0
+    for score in individualScores {
+        if score > 50 {
+            teamScore += 3
+        } else {
+            teamScore += 1
+        }
     }
-    else {
-        teamScore += 1
-    }
-    }
+    print(teamScore)
+}
 
-print(teamScore)
+func main() {
+    fruits()
+    controlFlow()
+}
+
+main()
