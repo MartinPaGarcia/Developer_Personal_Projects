e61638c39595f8b51ed647c40f38baa8ebaf3409
MartinPaGarcia <a01066569@itesm.mx>
2023-11-28 23:21:29 -0600

    Fix typo in conclusion text

--- Python/Manim/escala.py
+++ Python/Manim/escala.py
@@ -1,28 +1,35 @@
 from manim import *
 
-class SocioeconomicSurveyBarChart(Scene):
+class Escala(Scene):
     def construct(self):
-        title = Text("Discriminación por nivel socioeconómico")
+        title = Text("Escala: 1-10 (1 = No integrado, 10 = integrado)")
         title.to_edge(UP)
 
         data = {
-            "Sí": 3,
-            "No": 23,
+            "3":3,
+            "4":1,
+            "5":2,
+            "6":2,
+            "7":4,
+            "8":8,
+            "9":3,
+            "10":3,
         }
 
         # Convertir las claves y los valores del diccionario en listas
         bar_names = list(data.keys())
         values = list(data.values())
-
-        colors = ["#FF0000", "#FFFF00", "#00FF00"]
+        
+        x_values = [key for key in data]
+        colors = ["#00FF00" if int(key) > 6 else "#FF0000" for key in x_values]
 
         bar_chart = BarChart(
             values=values,
             bar_names=bar_names,
             bar_colors=colors,
-            y_range=[0, 25, 5],
+            y_range=[0, 10, 1],
             y_length=5,
-            x_length=5
+            x_length=10
         )
 
         self.play(Write(title))
@@ -33,10 +40,10 @@ class SocioeconomicSurveyBarChart(Scene):
         self.play(Write(labels))
         self.wait(1)
 
-        conclusion = Text("La mayoria se siente mayormente comoda", color=WHITE)
+        conclusion = Text("La mayoria se siente mayormente integrado", color=WHITE)
         conclusion.next_to(bar_chart, DOWN)
         self.play(Write(conclusion))
-        self.wait(2)
+        self.wait(10)
         self.play(FadeOut(title))
         self.play(FadeOut(labels), FadeOut(bar_chart))
 
